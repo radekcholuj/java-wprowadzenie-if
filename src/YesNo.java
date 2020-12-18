490975f37b6551cae7c6c21563f5506e49804d8b
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-18 21:50:55 +0100

    #7 przechowanie 5 liczb losowych i wyświetlenie w zalezności od wynik Tak lub Nie

---
+++ src/YesNo.java
@@ -0,0 +1,43 @@
+public class YesNo {
+    public static void main(String[] args) {
+
+        int number1 = -2;
+        int number2 = 4;
+        int number3 = -6;
+        int number4 = 8;
+        int number5 = -3;
+
+        if((number1*2) + 4 < 8){
+            System.out.println("Tak");
+        }else if((number1*2) + 4 >=8){
+            System.out.println("Nie");
+        }
+
+        if((number2*2) + 4 < 8){
+            System.out.println("Tak");
+        }else if((number2*2) + 4 >=8){
+            System.out.println("Nie");
+        }
+
+        if((number3*2) + 4 < 8){
+            System.out.println("Tak");
+        }else if((number3*2) + 4 >=8){
+            System.out.println("Nie");
+        }
+
+        if((number4*2) + 4 < 8){
+            System.out.println("Tak");
+        }else if((number4*2) + 4 >=8){
+            System.out.println("Nie");
+        }
+
+        if((number5*2) + 4 < 8){
+            System.out.println("Tak");
+        }else if((number5*2) + 4 >=8){
+            System.out.println("Nie");
+        }
+
+    }
+}
+
+
